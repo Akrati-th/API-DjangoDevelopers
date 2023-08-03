dd7132b561101be42cacf8229182b1bad711651a
AKRATI JADON <78207021+Akrati-th@users.noreply.github.com>
2023-08-04 01:09:06 +0530

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import developerDetail, developerDetailbyname
+from .views import *
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView
@@ -8,10 +8,9 @@ from rest_framework_simplejwt.views import (
 
 urlpatterns = [
     path('', views.appendix, name='appendix'),
-    path('developers/', views.developerList, name='list'),
-    path('developers-create/', views.developerCreate, name='create'),
-    path('developers/<int:pk>/', developerDetail.as_view(), name='detail'),
-    path('developers/<str:name>/', developerDetailbyname.as_view(), name='detail-name'),
-
+    path('developers/', get_all_developer_list, name='developers_list'),
+    path('developers-create/', create_new_developer, name='create'),
+    path('developers/<int:pk>/', developer_info.as_view(), name='detail'),
+    
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
 ]
