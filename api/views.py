0df29c210f4987a08c796ed98caac3f5f1e353ae
AKRATI JADON <78207021+Akrati-th@users.noreply.github.com>
2023-08-04 01:09:39 +0530

    Update views.py

--- api/views.py
+++ api/views.py
@@ -2,8 +2,11 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
+from django.http import HttpResponse
+from rest_framework import status
+import json
 
-from .serializers import DevSerializer
+from .serializers import ViewDeveloperSerializer, CreateDeveloperSerializer
 from .models import Developer
 
 
@@ -14,59 +17,45 @@ def appendix(request):
 
 
 @api_view(['GET'])
-def developerList(request):
-    dev = Developer.objects.all()
-    serializer = DevSerializer(dev, many=True)
-    return Response(serializer.data)
+@permission_classes([IsAuthenticated])
+def get_all_developer_list(request):
+    try:
+        dev = Developer.objects.all()
+        serializer = ViewDeveloperSerializer(dev, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": "Something went Wrong."}), status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
 @permission_classes([IsAuthenticated])
-def developerCreate(request):
-    serializer = DevSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-    return Response(serializer.data)
+def create_new_developer(request):
+    try:
+        serializer = CreateDeveloperSerializer(data=request.data)
+        if not serializer.is_valid():
+            return HttpResponse(json.dumps({"error": "Something went Wrong."}), status=status.HTTP_400_BAD_REQUEST)
+        new_serializer = serializer.create(serializer.validated_data)
+        new_serializer = ViewDeveloperSerializer(new_serializer).data
+        return HttpResponse(json.dumps({"developer": new_serializer}))
+    except Exception as e:
+        return HttpResponse(json.dumps({"error": "Something went Wrong."}), status=status.HTTP_400_BAD_REQUEST)
 
 
-class developerDetail(APIView):
+class developer_info(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, pk):
-        dev = Developer.objects.get(id=pk)
-        serializer = DevSerializer(dev, many=False)
-        return Response(serializer.data)
-
-    def put(self, request, pk):
-        dev = Developer.objects.get(id=pk)
-        serializer = DevSerializer(instance=dev, data=request.data)
-        return Response(serializer.data)
+        try:
+            dev = Developer.objects.get(id=pk)
+            serializer = ViewDeveloperSerializer(dev, many=False)
+            return Response(serializer.data)
+        except Exception as e:
+            return HttpResponse(json.dumps({"error": "Something went Wrong."}), status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk):
-        dev = Developer.objects.get(id=pk)
-        dev.delete()
-        return Response("Developer was Deleted!")
-
-
-class developerDetailbyname(APIView):
-    permission_classes = [IsAuthenticated]
-
-    def get(self, request, name):
-        dev = Developer.objects.get(name=name)
-        serializer = DevSerializer(dev, many=False)
-        return Response(serializer.data)
-
-    def put(self, request, name):
-        dev = Developer.objects.get(name=name)
-        serializer = DevSerializer(instance=dev, data=request.data)
-        return Response(serializer.data)
-
-    def delete(self, request, name):
-        dev = Developer.objects.get(name=name)
-        dev.delete()
-        return Response("Developer was Deleted!")
-
-
-
-
-
+        try:
+            dev = Developer.objects.get(id=pk)
+            dev.delete()
+            return Response("Developer was Deleted!")
+        except Exception as e:
+            return HttpResponse(json.dumps({"error": "Something went Wrong."}), status=status.HTTP_400_BAD_REQUEST)
