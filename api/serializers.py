685af0b7859ff27a9edb45c8fb97187e48c09059
AKRATI JADON <78207021+Akrati-th@users.noreply.github.com>
2023-08-04 01:07:45 +0530

    Update serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -2,7 +2,19 @@ from rest_framework.serializers import ModelSerializer
 from .models import Developer
 
 
-class DevSerializer(ModelSerializer):
+class ViewDeveloperSerializer(ModelSerializer):
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        data["developer_type"] = instance.developer_type.name
+        data["skills"] = instance.developer_type.developer_skill
+        return data
+
     class Meta:
         model = Developer
+        exclude = "developer_type"
+
+
+class CreateDeveloperSerializer(ModelSerializer):
+    class Meta:
+        models = Developer
         fields = '__all__'
